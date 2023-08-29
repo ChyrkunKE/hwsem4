eb92eb62b5c48c2197e2c05b3a89a65134a1fe9e
Katsiara <katsiara@mail.ru>
2023-08-29 19:58:55 +0300

    Второе

--- function1/Program.cs
+++ function1/Program.cs
@@ -7,7 +7,10 @@ int SumNumbers(int num)
     int sum = 0;
     for (int i = 1; i <= num; i++)
     {
+        checked
+        {
         sum += i; // sum = sum + 1
+        }
     }
     return sum;
 }
